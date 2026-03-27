7422dce274eeeeff3b486d8620a39cd5c84591c8
jmw0818 <wjdauddnjs12345@gmail.com>
2026-03-27 15:37:45 +0900

    init: spring boot project

---
+++ src/main/java/com/example/highpass_backend/HighpassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.highpass_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighpassBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighpassBackendApplication.class, args);
+	}
+
+}
